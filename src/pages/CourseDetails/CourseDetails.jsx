ec2086a5793b582da60253c496bc35419be8fb71
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-12-24 00:03:55 +0600

    fixed: // Enrollment mutation

--- src/pages/CourseDetails/CourseDetails.jsx
+++ src/pages/CourseDetails/CourseDetails.jsx
@@ -27,10 +27,7 @@ const CourseDetail = () => {
 
   // Enrollment mutation
   const loggedInUser = user;
-<<<<<<< HEAD
 
-=======
->>>>>>> 0b53ed0e770e6ee4749249abf66919e609dc98c0
   const enrollMutation = useMutation({
     mutationFn: async batch => {
       return axiosSecure.post('/enroll', {
